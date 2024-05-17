afbce3a6af533510051058054957fcaa70660439
junesyi <junetheyi@gmail.com>
2024-05-16 22:41:24 -0700

    adding environment variables and component for case insensitivity

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,24 +16,35 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
+// Box<dyn Error> is a trait object
+// the function will return a type that implements the 'Error' trait
+// 'dyn' keyword is short for dynamic, when we don't want to specify the particular type
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // Box<dyn Error> is a trait object
-    // the function will return a type that implements the 'Error' trait
-    // 'dyn' keyword is short for dynamic, when we don't want to specify the particular type
-    let contents =
-        // fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-        fs::read_to_string(config.file_path)?;
+    let contents = fs::read_to_string(config.file_path)?;
+    // fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
     // replaced the .expect() with ? operator
     // rather than 'panic!' on an error, '?' will return the error value from the current function for the caller
     // to handle
     // from std::fs library, read_to_string() takes an argument, opens the file, and returns a
     // std::io::Result<String> of the file's contents
-    for line in search(&config.query, &contents) {
+    for line in results {
         println!("{line}");
     }
 
@@ -55,18 +68,49 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+// to_lowercase(), lowercases the query string and assign it to query
+// calling to_lowercase() creates new data rather than referencing existing data, it's now a String rather than str
+// because its now a String, when we pass it into the for loop below, we add a '&' because 'contains()' only takes string slices
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
